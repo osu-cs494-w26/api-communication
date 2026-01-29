94fcb986dc3b10d9de15e5754d0b158e54b35392
Rob Hess <hessro@oregonstate.edu>
2026-01-29 15:32:50 -0800

    Add code from 1/29 lecture.

--- src/routes/FetchGet.jsx
+++ src/routes/FetchGet.jsx
@@ -1,13 +1,64 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
+
+import Spinner from '../components/Spinner'
+import ErrorContainer from '../components/ErrorContainer'
 
 export default function FetchGet() {
     const { org } = useParams()
+    const [ repos, setRepos ] = useState([])
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState(null)
+
+    useEffect(() => {
+        const controller = new AbortController()
+        async function fetchOrgRepos() {
+            setLoading(true)
+            try {
+                const res = await fetch(
+                    `https://api.github.com/orgs/${org}/repos`,
+                    { signal: controller.signal }
+                )
+                // if (res.status !== 200) {
+                //     setError()
+                // }
+                const resBody = await res.json()
+                console.log("== resBody:", resBody)
+                setLoading(false)
+                setError(null)
+                setRepos(resBody || [])
+            } catch (err) {
+                if (err.name === "AbortError") {
+                    console.log("HTTP request was cancelled")
+                } else {
+                    console.error(err)
+                    setError(err)
+                }
+            }
+        }
+
+        if (org) {
+            fetchOrgRepos()
+        }
+
+        return () => controller.abort()
+    }, [ org ])
 
     return (
         <>
             <h2>GitHub Repos for {org}</h2>
             <main>
-                {/* Render repos here... */}
+                {loading && <Spinner />}
+                {error && <ErrorContainer>{error.message}</ErrorContainer>}
+                <ul>
+                    {repos.map(repo => (
+                        <li key={repo.id}>
+                            <a href={repo.html_url}>
+                                {repo.full_name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
             </main>
         </>
     )
--- src/routes/FetchPost.jsx
+++ src/routes/FetchPost.jsx
@@ -5,11 +5,32 @@ const userId = 1234
 export default function FetchPost() {
     const [ title, setTitle ] = useState("")
     const [ body, setBody ] = useState("")
+
+    async function sendPost() {
+        if (userId && title && body) {
+            const res = await fetch(
+                "https://jsonplaceholder.typicode.com/posts",
+                {
+                    method: "POST",
+                    body: JSON.stringify({
+                        title: title,
+                        body: body,
+                        userId: userId
+                    }),
+                    headers: { "Content-Type": "application/json" }
+                }
+            )
+            const resBody = await res.json()
+            console.log("== resBody:", resBody)
+        }
+    }
+
     return (
         <>
             <h1>HTTP POST with fetch()</h1>
             <form onSubmit={e => {
                 e.preventDefault()
+                sendPost()
             }}>
                 <div>
                     <input placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} />
